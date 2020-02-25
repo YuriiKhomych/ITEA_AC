15672a4723aae15dc55da50f32210308236bef78
Sergii <svvay@ukr.net>
2020-02-25 14:26:37 +0200

    'in_work_keys'

---
+++ Davydenko_Sergii/l_4_iterators/baseinsight.py
@@ -0,0 +1,54 @@
+class BaseInsight(dict):
+    def __init__(self,
+                 metric_name=None,
+                 api=None,
+                 report_name=None,
+                 objective=None,
+                 unit=None,
+                 currency=None,
+                 id=None,
+                 validator_insight_type=None,
+                 metric_summary=None,
+                 **kwargs,
+                 ):
+        self.api = api
+        self.metric_name = metric_name
+        self.report_name = report_name
+        self.objective = objective
+        self._unit = unit
+        self._currency = currency
+        self.id = id
+        self.validator_insight_type = validator_insight_type
+        self.metric_summary = metric_summary
+        self.metrics = self.metric_summary
+
+    def to_dict(self):
+        return self.__dict__
+
+    @classmethod
+    def from_dict(cls, insights):
+        return cls(**insights)
+
+    def get_item(self, key):
+        return self.metrics[key]
+
+    def set_item(self, key, value):
+        self.metrics[key] = value
+
+    def del_item(self, key):
+        del self.metrics[key]
+
+    # try:
+    #     def __delitem__(self, key):
+    #         value = super().pop(key)
+    #         super().pop(value, None)
+    # except Exception as error:
+    #     print(f'{error}')
+    #
+    #
+    # def __setitem__(self, key, value):
+    #     if key in self:
+    #         del self[self[key]]
+    #     if value in self:
+    #         del self[value]
+    #     super().__setitem__(key, value)
---
+++ Davydenko_Sergii/l_4_iterators/hom_5 + 6.py
@@ -0,0 +1,18 @@
+first_list = [2, 3, 4, 5]
+second_list = [9, 8, 7, 6]
+
+# 5 cond
+def accepts_list(first_list, second_list):
+    return list(zip(first_list, second_list))
+print(accepts_list(first_list, second_list))
+
+
+# 6 cond
+
+def generator_squares(first_list, second_list):
+    result = []
+    result.append(list(i ** g for i in first_list for g in second_list))
+    return result
+
+print(generator_squares(first_list, second_list))
+
