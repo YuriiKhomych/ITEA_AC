5900c425ed8a2657b9403362f339a1e2db30f265
SalvadorSakho <Salvador.Sakho21@gmail.com>
2020-03-02 22:00:14 +0200

    added couple functons for tasks: 3, 4, 5

--- Salvador_Sakho/l_9_sql/Home_work/create_db_structure.sql
+++ Salvador_Sakho/l_9_sql/Home_work/create_db_structure.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.categories
 (
-    categoryid Serial Primary Key,
+    category_id Serial Primary Key,
     categoryname character varying(50) COLLATE pg_catalog."default",
     description text COLLATE pg_catalog."default"
 );
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS public.categories
 
 CREATE TABLE IF NOT EXISTS public.customers
 (
-    customerid Serial Primary Key,
+    customer_id Serial Primary Key,
     customername character varying(150) COLLATE pg_catalog."default",
     contactname character varying(150) COLLATE pg_catalog."default",
     address character varying(250) COLLATE pg_catalog."default",
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS public.customers
 
 CREATE TABLE IF NOT EXISTS public.employees
 (
-    employeeid Serial Primary Key,
+    employee_id Serial Primary Key,
     lastname character varying(155) COLLATE pg_catalog."default",
     firstname character varying(155) COLLATE pg_catalog."default",
     birthdate date,
@@ -31,14 +31,14 @@ CREATE TABLE IF NOT EXISTS public.employees
 
 CREATE TABLE IF NOT EXISTS public.orderdetails
 (
-    orderid Serial Primary Key,
+    order_id Serial Primary Key,
     productid integer,
     quantity integer
 );
 
 CREATE TABLE IF NOT EXISTS public.orders
 (
-    customerid Serial Primary Key,
+    customer_id Serial Primary Key,
     employeeid integer,
     orderdate date,
     shipperid integer
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS public.orders
 
 CREATE TABLE IF NOT EXISTS public.products
 (
-    productid Serial Primary Key,
+    product_id Serial Primary Key,
     productname character varying(255) COLLATE pg_catalog."default",
     supplierid integer,
     categoryid integer,
@@ -65,7 +65,7 @@ CREATE TABLE IF NOT EXISTS public.shippers
 
 CREATE TABLE IF NOT EXISTS public.suppliers
 (
-    supplierid Serial Primary Key,
+    supplier_id Serial Primary Key,
     suppliername character varying(50) COLLATE pg_catalog."default",
     contactname character varying(50) COLLATE pg_catalog."default",
     address character varying(50) COLLATE pg_catalog."default",
--- Salvador_Sakho/l_9_sql/Home_work/main.py
+++ Salvador_Sakho/l_9_sql/Home_work/main.py
@@ -1,26 +1,86 @@
 from Salvador_Sakho.l_9_sql.Home_work.sql_connection import connection_factory
 from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import re
 
 
+# task 5
+def delete_data(db_name, table_name, condition_column, condition):
+    connection_object = connection_factory(db_name)
+    cursor = connection_object.cursor()
+    condition = condition if isinstance(condition, int) \
+        else "'" + condition + "'"
+    cursor.execute(
+        f"delete from {table_name} "
+        f"where {condition_column} = {condition}"
+    )
+    cursor.close()
+    connection_object.commit()
 
-def task3(connection, db_name):
-    pass
 
-def task2(connection, db_name):
-    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    cursor = connection.cursor()
+# task 4
+def update_data(db_name, table_name, column_name, value
+                , condition_column, condition):
+    connection_object = connection_factory(db_name)
+    cursor = connection_object.cursor()
+    condition = condition if isinstance(condition, int) \
+        else "'" + condition + "'"
+    value = value if isinstance(value, int) \
+        else "'" + value + "'"
+    cursor.execute(
+        f"update {table_name} "
+        f"set {column_name} = {value}"
+        f"where {condition_column} = {condition}"
+    )
+    cursor.close()
+    connection_object.commit()
+
+
+# task 3
+def insert_data(db_name, table_name, data_collection):
+    connection_object = connection_factory(db_name)
+    cursor = connection_object.cursor()
+    cursor.execute(f"Select * FROM {table_name} LIMIT 0")
+    column_names_list = \
+        [
+            desc[0] for desc in cursor.description
+            if re.search('_id', desc[0]) is None
+        ]
+    column_names = ', '.join(column_names_list)
+    query_string = f"""INSERT INTO {table_name} ({column_names}) values (%s"""
+    i = 1
+    while i < len(data_collection[0]):
+        query_string += ',%s'
+        i += 1
+
+    query_string += ')'
+    cursor.executemany(query_string, data_collection)
+    cursor.close()
+    connection_object.commit()
+
+
+# task 2
+def create_db(db_name):
+    connection_object = connection_factory()
+    connection_object.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = connection_object.cursor()
     cursor.execute(sql.SQL(f"CREATE DATABASE {db_name}"))
-    connection = connection_factory(db_name)
-    cursor = connection.cursor()
+    connection_object = connection_factory(db_name)
+    cursor = connection_object.cursor()
     cursor.execute(open("create_db_structure.sql", "r").read())
     cursor.close()
-    connection.commit()
+    connection_object.commit()
+    connection_object.close()
 
 
 def main():
-    connection = connection_factory()
-    task2(connection, 'my_shop_test')
+    # create_db(connection, 'my_shop_test')
+    # insert_data('my_shop_test', 'categories',
+    #             (['some category name', 'some ditch']
+    #              , ['some another category name', 'some another ditch']))
+    # update_data('my_shop_test', 'categories', 'categoryname'
+    # , 'New column name' ,'category_id', 2)
+    delete_data('my_shop_test', 'categories', 'category_id', 2)
 
 
 if __name__ == '__main__':
