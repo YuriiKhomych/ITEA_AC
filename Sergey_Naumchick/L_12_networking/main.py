c3e5f49df1619642162033af7b5433755946e040
Sergey Naumchick <sergey.naumchick@gmail.com>
2020-03-14 15:52:50 +0200

    start working on L_12

---
+++ Sergey_Naumchick/L_12_networking/main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+
+def decorate(func):
+    def wrapper(name, site):
+        with open("answer.txt", "w") as file:
+            a = func(site)
+            file.write(f'{name} is - {a}')
+        print("operation success!")
+
+    return wrapper
+
+
+@decorate
+def ret_header(name, site):
+    response = requests.get(site)
+    soup = BS(response.content, 'html.parser')
+
+    titles = soup.find(name)
+    return titles.text
+
+
+if __name__ == '__main__':
+    ret_header('titlesss', 'title', 'https://stackoverflow.com')
