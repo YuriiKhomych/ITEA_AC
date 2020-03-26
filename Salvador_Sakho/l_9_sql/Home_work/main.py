bf78a44b7ca65e6d64b2b9b2d594927a713b5716
SalvadorSakho <salvador.sakho21@gmail.com>
2020-03-26 22:17:24 +0200

    Added context manger, refactored code according to changes

--- Salvador_Sakho/l_9_sql/Home_work/main.py
+++ Salvador_Sakho/l_9_sql/Home_work/main.py
@@ -1,34 +1,33 @@
-from Salvador_Sakho.l_9_sql.Home_work.sql_connection import connection_factory
+from Salvador_Sakho.l_9_sql.Home_work.sql_connection import \
+    ConnectionFactoryClass
 from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import re
 
 
 # task 7
-def select_join(db_name, table_1, table_2, column_tab_1, column_tab_2):
-    connection_object = connection_factory(db_name)
-    cursor = connection_object.cursor()
-    cursor.execute(sql.SQL(f'select * '
-                           f'from {table_1} as tab1 '
-                           f'join '
-                           f'{table_2} as tab2 '
-                           f'on tab1.{column_tab_1} = tab2.{column_tab_2}'))
+def select_join(connection, table_1, table_2, column_tab_1, column_tab_2):
+    cursor = connection.cursor()
+    cursor.execute(
+        sql.SQL(f'select * '
+                f'from {table_1} as tab1 '
+                f'join '
+                f'{table_2} as tab2 '
+                f'on tab1.{column_tab_1} = tab2.{column_tab_2}')
+    )
     result_set = cursor.fetchall()
     cursor.close()
-    connection_object.close()
     return result_set
 
 
 # task 6
-def multiple_selects(db_name, table_1, columns='*', where_condition=''):
-    connection_object = connection_factory(db_name)
-    cursor = connection_object.cursor()
+def multiple_selects(connection, table_1, columns='*', where_condition=''):
+    cursor = connection.cursor()
     cursor.execute(sql.SQL(f'select {columns} '
                            f'from {table_1} as tab1 '
                            f'{where_condition}'))
     result_set = cursor.fetchall()
     cursor.close()
-    connection_object.close()
     return result_set
 
 
@@ -36,7 +35,7 @@ def multiple_selects(db_name, table_1, columns='*', where_condition=''):
 # Нету смысла выписывать 5 разных видов селекта
 # , когда мы можем дать возможность пользователю
 # самому поределять какой селект он хочет
-def select_data(db_name, query=''):
+def select_data(connection, query=''):
     if any(
             word for word in query.split(' ')
             if (
@@ -48,19 +47,16 @@ def select_data(db_name, query=''):
             'Данный метод предназначен исключительно для получения данных'
         )
     else:
-        connection_object = connection_factory(db_name)
-        cursor = connection_object.cursor()
+        cursor = connection.cursor()
         cursor.execute(sql.SQL(query))
         result_set = cursor.fetchall()
         cursor.close()
-        connection_object.close()
         return result_set
 
 
 # task 5
-def delete_data(db_name, table_name, condition_column, condition):
-    connection_object = connection_factory(db_name)
-    cursor = connection_object.cursor()
+def delete_data(connection, table_name, condition_column, condition):
+    cursor = connection.cursor()
     condition = condition if isinstance(condition, int) \
         else "'" + condition + "'"
     cursor.execute(
@@ -68,14 +64,13 @@ def delete_data(db_name, table_name, condition_column, condition):
         f"where {condition_column} = {condition}"
     )
     cursor.close()
-    connection_object.commit()
+    connection.commit()
 
 
 # task 4
-def update_data(db_name, table_name, column_name, value
+def update_data(connection, table_name, column_name, value
                 , condition_column, condition):
-    connection_object = connection_factory(db_name)
-    cursor = connection_object.cursor()
+    cursor = connection.cursor()
     condition = condition if isinstance(condition, int) \
         else "'" + condition + "'"
     value = value if isinstance(value, int) \
@@ -86,13 +81,12 @@ def update_data(db_name, table_name, column_name, value
         f"where {condition_column} = {condition}"
     )
     cursor.close()
-    connection_object.commit()
+    connection.commit()
 
 
 # task 3
-def insert_data(db_name, table_name, data_collection):
-    connection_object = connection_factory(db_name)
-    cursor = connection_object.cursor()
+def insert_data(connection, table_name, data_collection):
+    cursor = connection.cursor()
     cursor.execute(f"Select * FROM {table_name} LIMIT 0")
     column_names_list = \
         [
@@ -105,41 +99,41 @@ def insert_data(db_name, table_name, data_collection):
     while i < len(data_collection[0]):
         query_string += ',%s'
         i += 1
-
     query_string += ')'
     cursor.executemany(query_string, data_collection)
+    connection.commit()
     cursor.close()
-    connection_object.commit()
 
 
 # task 2
 def create_db(db_name):
-    connection_object = connection_factory()
-    connection_object.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    cursor = connection_object.cursor()
-    cursor.execute(sql.SQL(f"CREATE DATABASE {db_name}"))
-    connection_object = connection_factory(db_name)
-    cursor = connection_object.cursor()
-    cursor.execute(open("create_db_structure.sql", "r").read())
-    cursor.close()
-    connection_object.commit()
-    connection_object.close()
+    with ConnectionFactoryClass() as connection:
+        connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cursor = connection.cursor()
+        cursor.execute(sql.SQL(f"CREATE DATABASE {db_name}"))
+        connection_object = connection(db_name)
+        cursor = connection_object.cursor()
+        cursor.execute(open("create_db_structure.sql", "r").read())
+        cursor.close()
+        connection_object.commit()
 
 
 def main():
-    # create_db(connection, 'my_shop_test')
-    # insert_data('my_shop_test', 'categories',
-    #             (['some category name', 'some ditch']
-    #              , ['some another category name', 'some another ditch']))
-    # update_data('my_shop_test', 'categories', 'categoryname'
-    # , 'New column name' ,'category_id', 2)
-    # delete_data('my_shop_test', 'categories', 'category_id', 2)
+    #create_db('my_shop_test')
+    with ConnectionFactoryClass('my_shop_test') as connection:
+        # insert_data(connection, 'categories',
+        #             [['2121 category name', 'some ditch']
+        #                 , ['some another category name', 'some another ditch']]
+        #             )
+        # update_data(connection, 'categories', 'categoryname'
+        # , 'New column name' ,'category_id', 24)
+        # delete_data(connection, 'categories', 'category_id', 23)
 
-    result_set = multiple_selects('my_shop_test', 'orders'
-                                  , 'customer_id, employeeid'
-                                  ,'where customer_id in  (1,2)')
-    for data in result_set:
-        print(data)
+        result_set = multiple_selects(connection, 'orders'
+                                      , 'customer_id, employeeid'
+                                      , 'where customer_id in  (1,2)')
+        for data in result_set:
+            print(data)
 
 
 if __name__ == '__main__':
--- Salvador_Sakho/l_9_sql/Home_work/sql_connection.py
+++ Salvador_Sakho/l_9_sql/Home_work/sql_connection.py
@@ -1,11 +1,23 @@
+import traceback
+
 import psycopg2
 
 
-def connection_factory(data_name=None):
-    try:
-        connection = psycopg2.connect(
-            dbname=data_name, user='postgres', password='admin'
-        )
-        return connection
-    except:
-        raise Exception()
+class ConnectionFactoryClass:
+    def __init__(self, data_name=None):
+        self.data_name = data_name
+
+    def __enter__(self):
+        try:
+            self.connection = psycopg2.connect(
+                dbname=self.data_name, user='postgres', password='admin'
+            )
+            return self.connection
+        except:
+            raise Exception()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            self.connection.close()
+        except Exception:
+            print(traceback.print_stack())
